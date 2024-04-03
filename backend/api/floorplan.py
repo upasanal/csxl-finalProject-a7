f94c4061182a046bf7dcada138fa390e2bbe9cf2
Upasana Lamsal <111466862+upasanal@users.noreply.github.com>
2024-04-02 23:15:27 -0400

    Sprint0 into Stage with Fixed API! (#30)
    * moved design document into doc folder
    
    * updated for all changes
    
    * added seatMapWidget files
    
    * Created component & corresponding routing for the CSXLSeatMap Component
    
    * Changed title of CSXLSeatMapComponent
    
    * Working on SVG Graphics for CSXL Seat Map
    
    * CSXL Seat Map Component (#24)
    
    * Created component & corresponding routing for the CSXLSeatMap Component
    
    * Changed title of CSXLSeatMapComponent
    
    * Working on SVG Graphics for CSXL Seat Map
    
    * Finished adding legend, reserve button, and finalized locations for seats
    
    * Seat Map Interactivity and Service (#25)
    
    * Created component & corresponding routing for the CSXLSeatMap Component
    
    * Changed title of CSXLSeatMapComponent
    
    * Working on SVG Graphics for CSXL Seat Map
    
    * Finished adding legend, reserve button, and finalized locations for seats
    
    * changes
    
    ---------
    
    Co-authored-by: Ellie Kim <elliekim100@gmail.com>
    
    * Confirmation Component Creation (#26)
    
    * Delete docs/design.md (#13)
    
    * Initialize design.md in docs (#14)
    
    * Delete design.md outside of docs (#15)
    
    * Created component & corresponding routing for the CSXLSeatMap Component
    
    * Changed title of CSXLSeatMapComponent
    
    * Working on SVG Graphics for CSXL Seat Map
    
    * confirmation component creation
    
    ---------
    
    Co-authored-by: Shreeya Kantamsetty <111532242+skanta04@users.noreply.github.com>
    Co-authored-by: ellieekimm <115567471+ellieekimm@users.noreply.github.com>
    Co-authored-by: Ellie Kim <elliekim100@gmail.com>
    
    * Completed Back End Implementation (#27)
    
    * Delete docs/design.md (#13)
    
    * Initialize design.md in docs (#14)
    
    * Delete design.md outside of docs (#15)
    
    * Created component & corresponding routing for the CSXLSeatMap Component
    
    * Changed title of CSXLSeatMapComponent
    
    * Working on SVG Graphics for CSXL Seat Map
    
    * Just to play around with front-end models
    
    * Playing with reservation
    
    * Yes
    
    * Working on Backend Models and Room_entity.py additions
    
    * Added API and Entities
    
    ---------
    
    Co-authored-by: Shreeya Kantamsetty <111532242+skanta04@users.noreply.github.com>
    Co-authored-by: ellieekimm <115567471+ellieekimm@users.noreply.github.com>
    Co-authored-by: Ellie Kim <elliekim100@gmail.com>
    Co-authored-by: upasanal <111466862+upasanal@users.noreply.github.com>
    
    * Console messages on service to properly debug and document
    
    * Fixing floorplan.py in API to have ri
    ght methods with good documentation
    
    ---------
    
    Co-authored-by: Jake Rogers <jmanerr@ad.unc.edu>
    Co-authored-by: Ellie Kim <elliekim100@gmail.com>
    Co-authored-by: ellieekimm <115567471+ellieekimm@users.noreply.github.com>
    Co-authored-by: Shreeya Kantamsetty <111532242+skanta04@users.noreply.github.com>
    Co-authored-by: Jake Rogers <97641772+Jakerogers1@users.noreply.github.com>

--- backend/api/floorplan.py
+++ backend/api/floorplan.py
@@ -14,7 +14,7 @@ from ..api.authentication import registered_user
 from ..models.user import User
 
 
-__authors__ = ["Ellie Kim, Shreeya Kantamsetty"]
+__authors__ = ["Ellie Kim, Shreeya Kantamsetty, Upasana Lamsal"]
 __copyright__ = "Copyright 2023"
 __license__ = "MIT"
 
@@ -23,128 +23,57 @@ openapi_tags = {
     "name": "Floorplan",
 }
 
-"""
-    2. Get the seats given a floorplan (floorplan.py)
-    3. get the tables given a floorplan (floorplan.py)
-    4. get the boundaries given a floorplans (floorplan.py)
-    5. put the seat coordinates for a seat given a seat biTCH (seat.py)
-"""
 
 
-@api.get("/{id}/r", response_model = list[Seat], tags = ["Floorplan"])
-def get_seats_by_floorplan(id: int, floorplan_service: FloorplanService = Depends(), ) -> list[Seat]:
+@api.get("/{id}/r", response_model=list[Seat], tags=["Floorplan"])
+def get_seats_by_floorplan(
+    id: int,
+    floorplan_service: FloorplanService = Depends(),
+) -> list[Seat]:
+    """Get seats by floorplan
+    Parameters:
+        id: a string representing a unique identifier for floorplam
+        floorplan_service: a valid floorplanService
+    """
     return floorplan_service.get_seats_by_id(id)
 
-@api.get("/{id}/boundary", response_model = str, tags = ["Floorplan"])
-def get_boundary_by_floorplan(id: int, floorplan_service: FloorplanService = Depends(), ) -> str:
+
+@api.get("/{id}/boundary", response_model=str, tags=["Floorplan"])
+def get_boundary_by_floorplan(
+    id: int,
+    floorplan_service: FloorplanService = Depends(),
+) -> str:
+    """Get boundary by a floorplan
+    Parameters:
+        id: a string representing a unique identifier for floorplam
+        floorplan_service: a valid floorplanService
+    """
     return floorplan_service.get_boundary_by_id(id)
 
-@api.get("/{id}/circle_tables", response_model = list[CircleTable], tags = ["Floorplan"])
-def get_circletables_by_floorplan(id: int, floorplan_service: FloorplanService = Depends(), ) ->list[CircleTable]:
+
+@api.get("/{id}/circle_tables", response_model=list[CircleTable], tags=["Floorplan"])
+def get_circletables_by_floorplan(
+    id: int,
+    floorplan_service: FloorplanService = Depends(),
+) -> list[CircleTable]:
+    """Get circle tables by floorplan
+    Parameters:
+        id: a string representing a unique identifier for floorplam
+        floorplan_service: a valid floorplanService
+    """
     return floorplan_service.get_circletables_by_id(id)
 
 
-@api.get("/{id}/circle_tables", response_model = list[RectangleTable], tags = ["Floorplan"])
-def get_rectangletables_by_floorplan(id: int, floorplan_service: FloorplanService = Depends(), ) -> list[RectangleTable]:
+@api.get("/{id}/circle_tables", response_model=list[RectangleTable], tags=["Floorplan"])
+def get_rectangletables_by_floorplan(
+    id: int,
+    floorplan_service: FloorplanService = Depends(),
+) -> list[RectangleTable]:
+    """Get rectangle tables by floorplan
+    Parameters:
+        id: a string representing a unique identifier for floorplam
+        floorplan_service: a valid floorplanService
+    """
     return floorplan_service.get_rectangletables_by_id(id)
 
 
-
-@api.get("", response_model=list[RoomDetails], tags=["Rooms"])
-def get_rooms(
-    room_service: RoomService = Depends(),
-) -> list[RoomDetails]:
-    """
-    Get all room
-
-    Parameters:
-        room_service: a valid RoomService
-
-    Returns:
-        list[RoomDetails]: All rooms in the `Room` database table
-    """
-    return room_service.all()
-
-
-@api.get(
-    "/{id}",
-    response_model=RoomDetails,
-    tags=["Rooms"],
-)
-def get_room_by_id(id: str, room_service: RoomService = Depends()) -> RoomDetails:
-    """
-    Get room with matching id
-
-    Parameters:
-        id: a string representing a unique identifier for a room
-        room_service: a valid RoomService
-
-    Returns:
-        RoomDetails: RoomDetails with matching slug
-    """
-
-    return room_service.get_by_id(id)
-
-
-@api.post("", response_model=RoomDetails, tags=["Rooms"])
-def new_room(
-    room: RoomDetails,
-    subject: User = Depends(registered_user),
-    room_service: RoomService = Depends(),
-) -> RoomDetails:
-    """
-    Create room
-
-    Parameters:
-        room: a valid room model
-        subject: a valid User model representing the currently logged in User
-        room_service: a valid RoomService
-
-    Returns:
-        RoomDetails: Created room
-    """
-
-    return room_service.create(subject, room)
-
-
-@api.put(
-    "",
-    response_model=RoomDetails,
-    tags=["Rooms"],
-)
-def update_room(
-    room: RoomDetails,
-    subject: User = Depends(registered_user),
-    room_service: RoomService = Depends(),
-) -> RoomDetails:
-    """
-    Update room
-
-    Parameters:
-        room: a valid Room model
-        subject: a valid User model representing the currently logged in User
-        room_service: a valid RoomService
-
-    Returns:
-        RoomDetails: Updated room
-    """
-
-    return room_service.update(subject, room)
-
-
-@api.delete("/{id}", response_model=None, tags=["Rooms"])
-def delete_room(
-    id: str,
-    subject: User = Depends(registered_user),
-    room_service: RoomService = Depends(),
-):
-    """
-    Delete room based on id
-
-    Parameters:
-        id: a string representing a unique identifier for an room
-        subject: a valid User model representing the currently logged in User
-        room_service: a valid RoomService
-    """
-
-    room_service.delete(subject, id)
